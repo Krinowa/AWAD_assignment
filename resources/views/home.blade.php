3d4522520dcc8849dfdc2fc5c0eac6e30c83179e
Czooi <126080176+Czooi@users.noreply.github.com>
2024-03-02 17:52:03 +0800

    Revert "asd"
    This reverts commit f8cdf4193c5592b4ab414818a2735844d4e10be6.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,6 @@
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Dashboarddddd</h1>
         <h1 class="h3 mb-0 text-gray-800">Dashboardd chi zhe</h1>
-        <h1 class="h3 mb-0 text-gray-800">Dashboardd chi zhe</h1>
         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                 class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
     </div>
